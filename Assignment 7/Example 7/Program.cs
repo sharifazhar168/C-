35b423192719469f33035aec184cd3d33841bd47
sharifahar168 <sharifahar168@gmail.com>
2021-06-10 16:30:13 +0500

    Added Assignment 7

---
+++ Assignment 7/Example 7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args) I
+        {
+            var numbers = new int[3];
+            numbers[0] = 1;
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+        }
+    }
+}
