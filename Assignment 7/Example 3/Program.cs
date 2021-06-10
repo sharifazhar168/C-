0673256b15dbf0a81562aa2953a2d772097d31ed
azhar <sharifazhar168@gmail.com>
2021-06-10 17:03:01 +0500

    Added Assignment 7

---
+++ Assignment 7/Example 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstname = "jazz";
+            var lastname = "mosh";
+            var fullname = firstname + "  " + lastname;
+            var myfullname = string.Format("my name is{0}{1}", firstname, lastname);
+            var names = new string[3] { "john", "jack", "mary" };
+            var formattednames = string.Join(",", names);
+            Console.WriteLine(formattednames);
+            Console.ReadLine();
+        
+        }
+    }
+}
---
+++ Assignment 7/Example 4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vs_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
