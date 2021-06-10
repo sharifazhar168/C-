185bd6055f4fe237e92d4cee916ed8da098d0f2b
sharifahar168 <sharifahar168@gmail.com>
2021-06-10 15:58:50 +0500

    Added Assignment 7

---
+++ Assignment 7/Example 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharpFundamentals
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+        public void Introduce()
+        {
+            Console.WriteLine("Hello ! , My name is " + firstName +" "+ lastName);
+        }
+    };
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Thomas = new Person();
+            Thomas.firstName = " Thomas";
+            Thomas.lastName = " Newt";
+            Thomas.Introduce();
+        }
+    }
+}
---
+++ Assignment 7/Example 2/Program.cs
@@ -0,0 +1,21 @@
+﻿ using System;
+
+namespace Calculator
+{
+    public class calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = calculator.Add(2, 5);
+            Console.WriteLine(result);
+
+        }
+    }
+}
